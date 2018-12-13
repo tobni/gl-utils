e90023f23e0259ad6b8f3137525b3948635bb770
Tobias Nilsson <t.obiasnilsson@telia.com>
2018-12-13 01:27:02 +0100

    vao is move constructible

--- include/vertex-array-object.h
+++ include/vertex-array-object.h
@@ -6,13 +6,15 @@
 #include <tuple>
 #include <string>
 #include <iostream>
+#include <algorithm>
+#include <utility>
 
 class VertexArrayObject {
 
     struct VertexBufferObject {
 
         template<typename T>
-        void init_buffer(gsl::span<T> data, GLint components, VertexArrayObject* vertex_array_object) {
+        void init_buffer(gsl::span<T> data, GLint components, VertexArrayObject*const vertex_array_object) {
             glBindVertexArray(vertex_array_object->id);
             glGenBuffers(1, &id);
             glBindBuffer(GL_ARRAY_BUFFER, id);
@@ -24,34 +26,76 @@ class VertexArrayObject {
         }
 
         private:
-        GLuint id;
+        GLuint id = 0;
     };
 
+    GLuint id = 0;
+    GLuint index_buffer_id = 0;
 
-    GLuint id;
-    GLuint index_buffer_object;
-    std::map<std::string, VertexBufferObject> vertex_buffer_objects;
+    template<typename T>
+    struct Less{
+
+    bool operator()(T const& lhs, T const& rhs) const 
+    {
+        return lhs<rhs;
+    }
+    };
+
+    std::map<std::reference_wrapper<std::string const>, VertexBufferObject, Less<std::string>> vertex_buffer_objects;
     
     public:
 
     VertexArrayObject() {
         glGenVertexArrays(1, &id);
     }
+
+    VertexArrayObject(VertexArrayObject const&) = delete;
+    VertexArrayObject(VertexArrayObject&& other)
+        : id{other.id}, index_buffer_id{other.index_buffer_id}, vertex_buffer_objects{std::move(other.vertex_buffer_objects)}
+    {
+        other.id = 0;
+        other.index_buffer_id = 0;
+    }
     
+    ~VertexArrayObject() {
+        remove_index_buffer();
+        glDeleteVertexArrays(1, &id);
+    }
+
     template<typename T>
-    void add_buffer(gsl::span<T> data, GLint components, std::string name) {
-        vertex_buffer_objects[name] = VertexBufferObject();
-        vertex_buffer_objects[name].init_buffer(data, components, this);
+    void add_buffer(gsl::span<T> data, GLint components, std::string const& name) {
+        vertex_buffer_objects.emplace(std::cref(name), VertexBufferObject());
+        vertex_buffer_objects[std::cref(name)].init_buffer(data, components, this);
+    }
+
+    void add_index_buffer(gsl::span<GLuint> data) {
+        glBindVertexArray(id);
+        glGenBuffers(1, &index_buffer_id);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer_id);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, data.size_bytes(), data.data(), GL_STATIC_DRAW);
     }
 
     void remove_buffer(std::string const& name) {
-        vertex_buffer_objects.erase(name);
+        vertex_buffer_objects.erase(std::cref(name));
     }
 
-    void print_buffers() {
-        for(auto const& kv : vertex_buffer_objects) {
-            std::cout << kv.first << " ";
+    void remove_index_buffer() {
+        glDeleteBuffers(1, &index_buffer_id);
+    }
+
+    GLuint const get_id() {
+        return id;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const VertexArrayObject& vao) {
+        os << "id=" << vao.id << " buffers= [";
+        char const* separator = "";
+        for (auto const& kv : vao.vertex_buffer_objects) {
+            auto const& name = kv.first; 
+            os << separator << name.get();
+            separator = ", ";
         }
-        std::cout << std::endl;
+        os << "]";
+        return os;
     }
 };
--- test/test-gl-utils.cc
+++ test/test-gl-utils.cc
@@ -1,7 +1,9 @@
 #include <catch2/catch.hpp>
 #include "../include/gl-utils.h"
 #include <iostream>
+#include <sstream>
 #include <vector>
+#include <array>
 using std::move;
 
 TEST_CASE( "Testing window factory function", "[window]" ) {
@@ -79,12 +81,42 @@ TEST_CASE( "Testing Input Manager key events", "[input-manager]" ) {
 
 TEST_CASE( "Testing VAO/VBO creaton", "[vertex-array-object]" ) {
     // For the context
-    auto window_option = make_window(600, 450, "Hello Factory");
+    auto window_option = make_window(600, 450, "Hello VAO");
     glewInit();
     //-----
 
     VertexArrayObject vao{};
-    std::vector<GLfloat> array{0.1, 0.2, 0.3};
-    vao.add_buffer(gsl::span(array.data(), array.size()), 1, "data");
-    vao.print_buffers();
+    std::array<GLfloat, 3> array_1{0.1f, 0.2f, 0.3f};
+    std::array<GLfloat, 6> array_2{0.5f, 0.3f, 0.1f, 0.6f, 0.4f, 0.6f};
+    std::array<std::string, 2> names{"data", "values"};
+
+    std::ostringstream out;
+    out << vao;
+    REQUIRE(out.str() == "id=" + std::to_string(vao.get_id()) + " buffers= []");
+    
+    vao.add_buffer(gsl::span(array_1.data(), array_1.size()), 1, names[0]);
+    vao.add_buffer(gsl::span(array_2.data(), array_2.size()), 2, names[1]);
+
+    std::ostringstream().swap(out);
+    out << vao;
+    REQUIRE(out.str() == "id=" + std::to_string(vao.get_id()) + " buffers= [data, values]");
+    
+    std::ostringstream().swap(out);
+    vao.remove_buffer(names[0]);
+    out << vao;
+    REQUIRE(out.str() == "id=" + std::to_string(vao.get_id()) + " buffers= [values]");
+
+    std::ostringstream().swap(out);
+    vao.remove_buffer(names[1]);
+    out << vao;
+    REQUIRE(out.str() == "id=" + std::to_string(vao.get_id()) + " buffers= []");
+
+    std::ostringstream().swap(out);
+    vao.add_buffer(gsl::span(array_1.data(), array_1.size()), 1, names[0]);
+    vao.add_buffer(gsl::span(array_2.data(), array_2.size()), 2, names[1]);
+
+    VertexArrayObject vao_2{std::move(vao)};
+    out << vao_2;
+    REQUIRE(out.str() == "id=" + std::to_string(vao_2.get_id()) + " buffers= [data, values]");
+
 }
